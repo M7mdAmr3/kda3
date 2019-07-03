c1fff3e81da4ff9655dbd1e9810d6087693a9935
M7mdAmr3 <43708078+M7mdAmr3@users.noreply.github.com>
2019-07-03 16:29:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,14 +4,14 @@ client.on('ready', function(){
  console.log(`Logged in as ${client.user.tag}!`);
     
    // var s = ['483063515981283354','483063446376677386','483063378726879232','483063354332545045','483063463179190293'];
-    var s = ['515797843198476289','515797696758546432','515797725334470656'];
+    var s = ['595982502846529536','595982501265276938','595982507489361931'];
     setInterval(function (){
     client.user.setPresence({
  game: { 
     type: 1,
-     url: 'https://www.twitch.tv/kosomkqmdhhh3',
+     url: 'https://www.twitch.tv/itsfakehehe',
     name: 'K/DA ! !',
-    application_id: '506847796687536143',
+    application_id: '595981556963737611',
      assets: {
          large_image:   `${s[Math.floor(Math.random() * s.length)]}`,
   
